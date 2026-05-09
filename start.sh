df391458cf8f868205c8c2e96cfe6eb5eaf969fc
Asshhv626 <u62606070@gmail.com>
2026-05-09 15:30:52 +0530

    Change pip3 to pip for package installation

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pip3 install python-gitlab python-telegram-bot telebot pymongo aiohttp requests && chmod +x laxxyscript.py && python3 laxxyscript.py
+pip install python-gitlab python-telegram-bot telebot pymongo aiohttp requests && chmod +x laxxyscript.py && python3 laxxyscript.py
